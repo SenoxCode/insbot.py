2a4c31f5e3d03e49ad236ab2492c95f5cd037650
Senox <53866400+SenoxCode@users.noreply.github.com>
2019-08-18 04:01:40 +0200

    Accounts lists for follower bot
    You can choose now different account lists for the follower bot.

--- insbot.py
+++ insbot.py
@@ -16,14 +16,14 @@ password = "password" #Change the password in the quotation marks to your Instag
 ##########
 
 pythonname = "insbot.py"
-version = 1.2
+version = 1.3
 
 
 time.sleep(0.3)
 ascii_banner = pyfiglet.figlet_format(pythonname + " " +  str(version))
 print(ascii_banner)
 time.sleep(1)
-print("\ninsbot.py 1.0 by Senox")
+print("\n" + pythonname + " "  + str(version) + " by Senox")
 time.sleep(0.3)
 print("https://github.com/SenoxCode\n\n\n")
 time.sleep(0.3)
@@ -36,9 +36,10 @@ print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 
 print("\n\n\n\n\n\nLogged in as " + name + "\n")
 
+germanaccounts = [165018, 601139763, 186926265, 266967359, 8826539, 631071319, 206446910, 340527886, 44451138, 49261384, 51577692, 192998906, 310056264, 446305980, 50326174, 378496191, 211563622, 483590318, 299374274, 733718680]
 popularaccounts = [6860189, 325734299, 305701719, 451573056, 5823711452, 787132, 11830955, 247944034, 232192182, 7719696, 25025320, 9365072543, 217338092, 13336763,187619120, 1574083, 182196805,748381723,19077223,  22686243]
 
-def followbot():
+def followbot(accountlist):
     print("\n\n\nThis bot will follow & unfollow popular accounts to get more followers on your account.")
     time.sleep(5)
     print("\nStarting bot...")
@@ -46,7 +47,7 @@ def followbot():
     time.sleep(3)
     while True:
         account = 1
-        for x in popularaccounts:
+        for x in accountlist:
             api.follow(x)
             xstring = str(x)
             print("Following UserID " + xstring + "..." + " [" +  str(account) +  "/20]")
@@ -64,7 +65,7 @@ def followbot():
         print("\nWaiting 120 seconds...\n")
         time.sleep(120)
 
-        for x in popularaccounts:
+        for x in accountlist:
             api.unfollow(x)
             xstring = str(x)
             print("Unfollowing UserID " + xstring + "..." + " [" +  str(account) +  "/20]")
@@ -134,7 +135,18 @@ if option == "0":
     print("\nExiting...")
     time.sleep(3)
 elif option == "1":
-    followbot()
+    print("\nChoose an option:\n[0] EXIT\n[1] International account list \n[2] German account list")
+    optionlist = raw_input()
+    if optionlist == "0":
+        print("Exiting...")
+        time.sleep(3)
+    elif optionlist == "1":
+        followbot(popularaccounts)
+    elif optionlist == "2":
+        followbot(germanaccounts)
+    else:
+        print("Unknown option...")
+        time.sleep(2)
 elif option == "2":
     likebot()
 elif option == "3":
