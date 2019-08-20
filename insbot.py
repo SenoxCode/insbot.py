e73384cd588206bf43cfc8f147be7b2b08a2bce2
Senox <53866400+SenoxCode@users.noreply.github.com>
2019-08-21 01:30:05 +0200

    Added safe & fast mode

--- insbot.py
+++ insbot.py
@@ -16,7 +16,7 @@ password = "password" #Change the password in the quotation marks to your Instag
 ##########
 
 pythonname = "insbot.py"
-version = 1.3
+version = 1.4
 
 
 time.sleep(0.3)
@@ -33,13 +33,15 @@ print("Trying to login...")
 api = InstagramAPI(name, password)
 api.login()
 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-
 print("\n\n\n\n\n\nLogged in as " + name + "\n")
+print("Slow and safemode (Better for longer sessions [+1 HOUR]) [s] | Fast, but riskmode to get blocked after a time (Better for short sessions [1-60 MINUTES]) [f]")
+mode = raw_input()
+print("\n\n")
 
 germanaccounts = [165018, 601139763, 186926265, 266967359, 8826539, 631071319, 206446910, 340527886, 44451138, 49261384, 51577692, 192998906, 310056264, 446305980, 50326174, 378496191, 211563622, 483590318, 299374274, 733718680]
 popularaccounts = [6860189, 325734299, 305701719, 451573056, 5823711452, 787132, 11830955, 247944034, 232192182, 7719696, 25025320, 9365072543, 217338092, 13336763,187619120, 1574083, 182196805,748381723,19077223,  22686243]
 
-def followbot(accountlist):
+def followbot(accountlist, followtime, unfollowtime):
     print("\n\n\nThis bot will follow & unfollow popular accounts to get more followers on your account.")
     time.sleep(5)
     print("\nStarting bot...")
@@ -51,7 +53,7 @@ def followbot(accountlist):
             api.follow(x)
             xstring = str(x)
             print("Following UserID " + xstring + "..." + " [" +  str(account) +  "/20]")
-            time.sleep(5)
+            time.sleep(followtime)
 
 
             if account < 20:
@@ -69,7 +71,7 @@ def followbot(accountlist):
             api.unfollow(x)
             xstring = str(x)
             print("Unfollowing UserID " + xstring + "..." + " [" +  str(account) +  "/20]")
-            time.sleep(3)
+            time.sleep(unfollowtime)
             if account < 20:
                 account += 1
             else:
@@ -78,7 +80,7 @@ def followbot(accountlist):
         print("\nWaiting 60 seconds...\n")
         time.sleep(60)
 
-def likebot():
+def likebot(liketime):
     print("\n\n\nThis bot will like other pictures to get you more likes.")
     time.sleep(5)
     print("\nStarting bot...")
@@ -90,9 +92,9 @@ def likebot():
         for item in tmp["items"]:
             api.like(item["id"])
             print("Liked MediaID " + item["id"])
-            time.sleep(5)
+            time.sleep(liketime)
 
-def likeandunlikebot():
+def likeandunlikebot(liketime, unliketime):
     print("\n\n\nThis bot will like & unlike pictures to get more likes on your account.")
     time.sleep(5)
     print("\nStarting bot...")
@@ -107,7 +109,7 @@ def likeandunlikebot():
             api.like(item["id"])
             print("Liked MediaID " + item["id"])
             loop = loop + 1
-            time.sleep(5)
+            time.sleep(liketime)
             likedpictures.append(item["id"])
 
             if loop == 25:
@@ -115,7 +117,7 @@ def likeandunlikebot():
                 for i in likedpictures:
                     api.unlike(i)
                     print("Unliked MediaID " + i)
-                    time.sleep(3)
+                    time.sleep(unliketime)
                 likedpictures.clear()
 
 
@@ -127,32 +129,58 @@ def likeandunlikebot():
 print("Choose an option:\n[0] EXIT\n[1] FOLLOWER BOT\n[2] LIKE BOT\n[3] LIKE & UNLIKE BOT")
 option = raw_input()
 
-
-
-
-
-if option == "0":
-    print("\nExiting...")
-    time.sleep(3)
-elif option == "1":
-    print("\nChoose an option:\n[0] EXIT\n[1] International account list \n[2] German account list")
-    optionlist = raw_input()
-    if optionlist == "0":
-        print("Exiting...")
+if mode == "f":
+    if option == "0":
+        print("\nExiting...")
         time.sleep(3)
-    elif optionlist == "1":
-        followbot(popularaccounts)
-    elif optionlist == "2":
-        followbot(germanaccounts)
+    elif option == "1":
+        print("\nChoose an option:\n[0] EXIT\n[1] International account list \n[2] German account list")
+        optionlist = raw_input()
+        if optionlist == "0":
+            print("Exiting...")
+            time.sleep(3)
+        elif optionlist == "1":
+            followbot(popularaccounts,6,3)
+        elif optionlist == "2":
+            followbot(germanaccounts, 6,3)
+        else:
+            print("Unknown option...")
+            time.sleep(2)
+    elif option == "2":
+        likebot(5)
+    elif option == "3":
+        likeandunlikebot(5, 3)
+
     else:
-        print("Unknown option...")
-        time.sleep(2)
-elif option == "2":
-    likebot()
-elif option == "3":
-    likeandunlikebot()
+        print("ERROR: INVALID NUMBER...")
+        time.sleep(3)
+
+elif mode == "s":
+    if option == "0":
+        print("\nExiting...")
+        time.sleep(3)
+    elif option == "1":
+        print("\nChoose an option:\n[0] EXIT\n[1] International account list \n[2] German account list")
+        optionlist = raw_input()
+        if optionlist == "0":
+            print("Exiting...")
+            time.sleep(3)
+        elif optionlist == "1":
+            followbot(popularaccounts, 29, 14)
+        elif optionlist == "2":
+            followbot(germanaccounts, 29, 14)
+        else:
+            print("Unknown option...")
+            time.sleep(2)
+    elif option == "2":
+        likebot(28)
+    elif option == "3":
+        likeandunlikebot(28, 14)
+
+    else:
+        print("ERROR: INVALID NUMBER...")
+        time.sleep(3)
 
 else:
-    print("ERROR: INVALID NUMBER...")
+    print("ERROR: MODE ISN'T VALIABLE...")
     time.sleep(3)
-
